7830b0b1f0e815f1e9bc8891f912ec80d366bd36
Isaac <isaac@blogfast.local>
2026-01-22 16:17:35 -0500

    Trigger clean deployment

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script sets up the BlogFast Django project by installing dependencies,
-# applying migrations, and creating a superuser.
-pip install -r requirements.txt
-python manage.py makemigrations
-python manage.py migrate
-python manage.py tailwindcss install
-python manage.py collectstatic --noinput
